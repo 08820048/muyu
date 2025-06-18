48f55425e2547e81a3d9a7680e239dfae9b4a02d
Ultimate Admin <admin@ultimate.com>
2025-06-18 10:04:17 +0800

    增强 escape.sh 脚本，处理更多可能的宏定义问题

--- escape.sh
+++ escape.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# 查找所有 Markdown 文件并替换 'fs' 为 'f_s'
-find content -name "*.md" -type f -exec sed -i 's/fs/f_s/g' {} \;
+# 查找所有 Markdown 文件并处理可能导致宏定义错误的内容
+find content -name "*.md" -type f -exec sed -i \
+  -e 's/fs/f_s/g' \
+  -e 's/{{/{ {/g' \
+  -e 's/}}/} }/g' \
+  -e 's/{%/{ %/g' \
+  -e 's/%}/% }/g' \
+{} \;
